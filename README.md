ba6f43a2d1aba3b0a966d7b259f629a086d3e92b
Koji Tanaka <ktanaka@c-brains.jp>
2016-04-10 03:15:04 +0900

    [Add] CakePHP Database Connection

--- README.md
+++ README.md
@@ -12,3 +12,10 @@ vagrant plugin install vagrant-multiplug
 cp itamae/node.default.yml itamae/node.yml
 vagrant up
 ```
+
+## provision in VM
+
+```
+vagrant ssh
+vagrant$ cd /var/itamae; sudo bundle exec itamae local /vagrant/itamae/bootstrap.rb --node-yaml=/vagrant/itamae/node.yml
+```
--- app/config/app.php
+++ app/config/app.php
@@ -216,20 +216,8 @@ return [
      */
     'Datasources' => [
         'default' => [
-            'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'localhost',
-            /**
-             * CakePHP will use the default DB port based on the driver selected
-             * MySQL on MAMP uses port 8889, MAMP users will want to uncomment
-             * the following line and set the port accordingly
-             */
-            //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
-            'encoding' => 'utf8',
+            'encoding' => 'utf8mb4',
             'timezone' => 'UTC',
             'flags' => [],
             'cacheMetadata' => true,
@@ -261,15 +249,8 @@ return [
          * The test connection is used during the test suite.
          */
         'test' => [
-            'className' => 'Cake\Database\Connection',
-            'driver' => 'Cake\Database\Driver\Mysql',
             'persistent' => false,
-            'host' => 'localhost',
-            //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'test_myapp',
-            'encoding' => 'utf8',
+            'encoding' => 'utf8mb4',
             'timezone' => 'UTC',
             'cacheMetadata' => true,
             'quoteIdentifiers' => false,
