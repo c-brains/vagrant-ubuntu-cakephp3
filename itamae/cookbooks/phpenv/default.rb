2eb9a8f40048a5945e33c8e31128312d07bb9346
Koji Tanaka <ktanaka@c-brains.jp>
2016-04-09 23:21:25 +0900

    [Add] php7.0-fpm

--- itamae/cookbooks/phpenv/default.rb
+++ itamae/cookbooks/phpenv/default.rb
@@ -3,6 +3,17 @@
 include_cookbook "ppa-ondrej-php70-repository"
 include_cookbook "nginx"
 
+# PHP
+%w(php7.0-fpm).each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+service "php7.0-fpm" do
+  action [:enable, :start]
+end
+
 template "/etc/nginx/sites-available/01-#{node['nginx']['default']['server_name']}.conf" do
   source "templates/etc/nginx/sites-available/app.conf.erb"
   mode '0644'
