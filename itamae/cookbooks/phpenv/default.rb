23c1e21ce333762e64ebf7ab9354eca8616675fc
Koji Tanaka <ktanaka@c-brains.jp>
2016-04-09 23:34:33 +0900

    [Add] install composer

--- itamae/cookbooks/phpenv/default.rb
+++ itamae/cookbooks/phpenv/default.rb
@@ -4,7 +4,7 @@ include_cookbook "ppa-ondrej-php70-repository"
 include_cookbook "nginx"
 
 # PHP
-%w(php7.0-fpm).each do |pkg|
+%w(php7.0-fpm php7.0-curl).each do |pkg|
   package pkg do
     action :install
   end
@@ -12,6 +12,24 @@ end
 
 service "php7.0-fpm" do
   action [:enable, :start]
+end
+
+# composer
+execute "composer install" do
+  command "curl -sS https://getcomposer.org/installer | php && mv composer.phar /usr/local/bin/composer"
+  not_if "test $(which /usr/local/bin/composer)"
+end
+
+directory "/home/vagrant/.composer" do
+  owner 'vagrant'
+  group 'vagrant'
+end
+
+# composer parallel install plugin https://github.com/hirak/prestissimo
+execute "prestissimo install (composer parallel install plugin)" do
+  user "vagrant"
+  command "composer global require hirak/prestissimo"
+  not_if ("composer global show -i 2>&1 | grep -qi hirak/prestissimo")
 end
 
 template "/etc/nginx/sites-available/01-#{node['nginx']['default']['server_name']}.conf" do
